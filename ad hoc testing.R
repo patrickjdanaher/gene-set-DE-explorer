5577ce8a5849777cff19663b05211992dc747d21
patrickjdanaher <patrickjdanaher@gmail.com>
2018-11-30 21:14:44 -0800

    added example data
    and added to ad hoc testing file

--- ad hoc testing.R
+++ ad hoc testing.R
@@ -1,12 +1,29 @@
 ##### ad-hoc testing of package functions:
 
 
-ests = -100 : 100
-pvals = runif(201, 0, 1)/pmax(pmin(abs(ests),50),1)
+## load permuted results from a real study:
+temp = read.csv("example data.csv", stringsAsFactors = F, row.names = 1)
+ests = temp[,"log2.fold.change.from.baseline"]
+pvals = temp[,"p.value"]
+fdrs = p.adjust(pvals,"BH")
+names = rownames(temp)
+volcplot(ests, pvals, names = names,fdrs = fdrs,
+            show.names.top.N = 100)#,   show.names.fdr.thresh = 0.05)
 
-pvals[abs(ests)>20] = pvals[abs(ests)>20]
+
+## load gene sets:
 
 
-volcplot(ests, pvals, names = paste0("gene",1:length(ests)),
-      #   show.names.top.N = 20,
-         show.names.fdr.thresh = 0.1)
+
+
+
+## run gene set plot:
+genesetplot(ests, pvals, names, genesets,
+            n.genesets = 10, min.geneset.size = 5, min.geneset.coverage = 0.3, 
+            mandatory.genesets = NULL, geneset.ranking.method = "most.significant",
+            fdr.lines = c(0.05, 0.5),
+            color.genes.up = "firebrick", color.genes.dn = "darkblue", 
+            color.background.up = rgb(1,0,0,0.2), color.background.dn = rgb(0,0,1,0.2),
+            xlab = "Estimate", ylim = NULL, 
+            cex.points = 0.5, cex.genenames = 0.5, cex.legend = 0.5)
+  
